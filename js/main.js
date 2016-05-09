4f9ffa73b7e6e10032e8936f0d95c8df2dcbb2f6
Stephen Chen <chen.stephen.r@gmail.com>
2016-05-09 12:29:50 -0700

    grid and files setup

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var View = function () {
+  this.setupGrid();
+};
+
+View.prototype.setupGrid = function () {
+  var li;
+  for (var rowIdx = 0; rowIdx < 4; rowIdx++) {
+    for (var colIdx = 0; colIdx < 4; colIdx++) {
+      li = document.createElement("li")
+    }
+  }
+};
