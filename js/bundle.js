6b4ea50c35785f7f4b07f3c55a88706083c7e55b
Stephen Chen <chen.stephen.r@gmail.com>
2016-05-09 14:17:49 -0700

    color stays the same if changed recently

--- js/bundle.js
+++ js/bundle.js
@@ -47,34 +47,41 @@
 	var colors = ['darksalmon', 'cornflowerblue', 'red', 'yellow', 'wheat',
 	  'hotpink', 'green', 'orange', 'purple', 'darkolivegreen', 'aqua'];
 	
-	var View = function () {
-	  this.setupGrid();
-	  setInterval(this.createRandomColor, 250);
-	};
 	
-	View.prototype.setupGrid = function () {
+	var setupGrid = function () {
 	  var ul = document.querySelector('ul');
 	  var li;
 	  for (var rowIdx = 0; rowIdx < 4; rowIdx++) {
 	    for (var colIdx = 0; colIdx < 4; colIdx++) {
 	      li = document.createElement("li");
 	      li.setAttribute('pos', [rowIdx, colIdx]);
+	      li.setAttribute('isChangedRecently', false);
 	      ul.appendChild(li);
 	    }
 	  }
 	};
 	
-	View.prototype.createRandomColor = function () {
+	var createRandomColor = function () {
 	  var rowIdx = Math.floor(Math.random() * 4);
 	  var colIdx = Math.floor(Math.random() * 4);
 	
 	  var randomSquare = document.querySelector('li[pos=\"' + rowIdx
 	                      + ',' + colIdx + '\"]');
-	  var randomColor = colors[Math.floor(Math.random() * colors.length)];
-	  randomSquare.style.background = randomColor;
+	
+	  if (randomSquare.getAttribute('isChangedRecently') === 'false') {
+	    var randomColor = colors[Math.floor(Math.random() * colors.length)];
+	    randomSquare.style.background = randomColor;
+	    randomSquare.setAttribute('isChangedRecently', true);
+	    setTimeout(function () {
+	      randomSquare.setAttribute('isChangedRecently', false);
+	    }, 2000)
+	  } else {
+	    createRandomColor();
+	  }
 	};
 	
-	new View();
+	setupGrid();
+	setInterval(createRandomColor, 250);
 
 
 /***/ }
