445e433af54c386ba2940b63b806dc22910814bf
Stephen Chen <chen.stephen.r@gmail.com>
2016-05-09 13:10:36 -0700

    grid is viewable

--- js/main.js
+++ js/main.js
@@ -3,10 +3,15 @@ var View = function () {
 };
 
 View.prototype.setupGrid = function () {
+  var ul = document.querySelector('ul');
   var li;
   for (var rowIdx = 0; rowIdx < 4; rowIdx++) {
     for (var colIdx = 0; colIdx < 4; colIdx++) {
-      li = document.createElement("li")
+      li = document.createElement("li");
+      li.setAttribute('pos', [rowIdx, colIdx]);
+      ul.appendChild(li);
     }
   }
 };
+
+new View();
